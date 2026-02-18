de37f2bc8533c3e4caabc6fb7dd2936db576a2bb
Ulloa <Ulloa@LAPTOP-C4A4PTCQ>
2026-02-18 11:35:28 -0600

    se le agregaron iconos y se programo por completo la funcionalidad de longitud

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.miprimeraapp;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
@@ -15,11 +17,11 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     TabHost tbh;
-    TextView temVal;
+    TextView tempVal;
     Spinner spn;
     Button btn;
-    Double valores[] = new Double[]{1.0, 0.85, 7.67, 26.42, 36.80, 495.77};
-
+    Double valores[] = new Double[] {1.0, 0.85, 7.67, 26.42, 36.80, 495.77};
+    Double longitudes[] = new Double[] {1.0, 1000.0, 100.0, 39.3701, 3.280841666667, 1.1963081929167, 1.09361};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,30 +30,49 @@ public class MainActivity extends AppCompatActivity {
         tbh = findViewById(R.id.tbhConversores);
         tbh.setup();
 
-        tbh.addTab(tbh.newTabSpec("Monedas").setContent(R.id.tabMonedas).setIndicator("MONEDAS", null));
-        tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
-        tbh.addTab(tbh.newTabSpec("Volumen").setContent(R.id.tabVolumen).setIndicator("VOLUMEN", null));
-        tbh.addTab(tbh.newTabSpec("Masa").setContent(R.id.tabMasa).setIndicator("MASA", null));
+        tbh.addTab(tbh.newTabSpec("Monedas").setContent(R.id.tabMonedas).setIndicator("", getDrawable(R.drawable.moneda)));
+        tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tabLongitud).setIndicator("", getResources().getDrawable(R.drawable.longitud)));
+        tbh.addTab(tbh.newTabSpec("Volumen").setContent(R.id.tabVolumen).setIndicator("", getDrawable(R.drawable.volumen)));
+        tbh.addTab(tbh.newTabSpec("Masa").setContent(R.id.tabMasa).setIndicator("", getDrawable(R.drawable.masa)));
 
-        btn = findViewById(R.id.btnMonedasCovertir);
-        btn.setOnClickListener(v -> covertirMonedas());
+        btn = findViewById(R.id.btnMonedasConvertir);
+        btn.setOnClickListener(v->convertirMonedas());
+
+        btn = findViewById(R.id.btnLongitudConvertir);
+        btn.setOnClickListener(v->convertirLongitud());
     }
+    private void convertirLongitud(){
+        spn = findViewById(R.id.spnLongitudDe);
+        int de = spn.getSelectedItemPosition();
 
-    private void covertirMonedas() {
+        spn = findViewById(R.id.spnLongitudA);
+        int a = spn.getSelectedItemPosition();
+
+        tempVal = findViewById(R.id.txtLongitudCantidad);
+        double cantidad = Double.parseDouble(tempVal.getText().toString());
+        double respuesta = conversorLongitud(de, a, cantidad);
+
+        tempVal = findViewById(R.id.lblLongitudRespuesta);
+        tempVal.setText("Respuesta: "+ respuesta);
+    }
+    private void convertirMonedas(){
         spn = findViewById(R.id.spnMonedasDe);
         int de = spn.getSelectedItemPosition();
 
         spn = findViewById(R.id.spnMonedasA);
         int a = spn.getSelectedItemPosition();
 
-        temVal = findViewById(R.id.txtMonedasCantidad);
-        double cantidad = Double.parseDouble(temVal.getText().toString());
-        double respuesta = coversor(de, a, cantidad);
+        tempVal = findViewById(R.id.txtMonedasCantidad);
+        double cantidad = Double.parseDouble(tempVal.getText().toString());
+        double respuesta = conversor(de, a, cantidad);
 
-        temVal = findViewById(R.id.lblMonedasRespuesta);
-        temVal.setText("Respuesta: " + respuesta);
+        tempVal = findViewById(R.id.lblMonedasRespuesta);
+        tempVal.setText("Respuesta: "+ respuesta);
     }
-    double coversor(int de, int a, double cantidad) {
-        return valores[a] / valores[de] * cantidad;
+    double conversor(int de, int a, double cantidad){
+        return valores[a]/valores[de] * cantidad;
+    }
+    double conversorLongitud(int de, int a, double cantidad){
+        return longitudes[a]/longitudes[de] * cantidad;
     }
 }
