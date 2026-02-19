9449848ce1b3bab973dde8c00a3a7f0b5a96e164
Ulloa <Ulloa@LAPTOP-C4A4PTCQ>
2026-02-18 18:52:01 -0600

    Laboratorio #2, conversores: Monedas, Masa, Volumen, Longitud, Almacenamiento, Tiempo, Transferencia de Datos.

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -16,14 +16,26 @@ public class MainActivity extends AppCompatActivity {
     Spinner spn;
     Button btn;
     Double valores[][] = {
-            {1.0, 0.85, 7.67, 26.42, 36.80, 495.77}, //moendas
+            {1.0, 0.85, 7.67, 26.42, 36.80, 495.77}, //monedas
+            {1.0, 1000.0, 2.20462, 35.274, 0.08818, 0.022046,}, //Masa
+            {1.0, 1000.0, 0.26417, 4.2267, 33.814}, //volumen
             {1.0, 1000.0, 100.0, 39.3701, 3.280841666667, 1.1963081929167, 1.09361}, //longitud
-            {}, //volumen
+            {1.0, 1024.0, 1048576.0, 1073741824.0, 1099511627776.0}, //almacenamiento
+            {1.0, 60.0, 3600.0, 86400.0, 604800.0, 2629746.0, 31556952.0}, //Tiempo
+            {1.0, 1000.0, 1000000.0, 1000000000.0, 125000.0, 125000000.0}, //Transferencia de Datos
+
+
+
     };
     String[][] etiquetas = {
             {"Dolar", "Euro", "Quetzal", "Lempira", "Cordoba", "Colon CR"}, //monedas
+            {"Kg", "Gramos", "Libra", "Onza", "Arrobas", "Quintales"},//Masa
+            {"Litro", "Mililitros", "Galon", "Taza", "Onza liq"}, //Volumen
             {"Mts", "Ml", "Cm", "Pulgada", "Pies", "Vara", "Yarda"}, //Longitud
-            {""},  //volumen
+            {"Byte", "Kilobyte", "Megabyte", "Gigabyte", "Terabyte"}, //Almacenamiento
+            {"Segundo", "Minuto", "Hora", "Día", "Semana", "Mes", "Año"}, //Tiempo
+            {"bits", "kilobits", "Megabits", "Gigabits", "Kilobytes", "Megabytes"}, //Transferencia de Datos
+
     };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
